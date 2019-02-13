26e6083b7bcda7964815cbc023d194fcf3bfd325
bpayanpsl <bpayanpsl@gmail.com>
2019-02-13 12:01:13 +0100

    tps

--- ihm.js
+++ ihm.js
@@ -1,7 +1,37 @@
 var service = require('./service');
+var readline = require('readline');
+var request = require('request');
 
-exports.start = function() {
-    service.init(function(nb) {
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+exports.start = function () {
+    service.init(function (nb) {
         console.log('[init]', nb, 'sessions trouvées.')
     });
+    menu();
 };
+var menu = function () {
+    rl.question("1. Rafraichir les données\n2. Lister les sessions\n99. Quitter\n", function (saisie) {
+        switch (saisie) {
+            case ('1'): console.log("blabla1\n")
+               // service.init(callback);
+                menu();
+                break;
+            case ('2'): console.log("blabla2\n")
+                service.init(function (nb) {
+                    console.log('[init]', nb, 'sessions trouvées.')
+                });
+                menu();
+                break;
+            case ('99'): console.log("blabla3\n")
+                rl.close();
+                break;
+        }
+    })
+}
+
+
+
